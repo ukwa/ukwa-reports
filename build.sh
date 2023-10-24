d9cbd32a4b9cac664f7a6366f225116b44341cc7
Andrew Jackson <Andrew.Jackson@bl.uk>
2023-10-24 20:56:07 +0100

    Add ability to copy into a place.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # https://discourse.jupyter.org/t/debugger-warning-it-seems-that-frozen-modules-are-being-used-python-3-11-0/16544
 export PYDEVD_DISABLE_FILE_VALIDATION=1
@@ -11,3 +11,12 @@ echo Copying CSV files from the content folder to the _build:
 cd content 
 find . -name "*.csv" -exec cp -v {} ../_build/html/{} \;
 cd -
+
+# Copy output to the OUTPUT_PATH if specified:
+if [[ -z "${OUTPUT_PATH}" ]]; then
+  echo No OUTPUT_PATH specified.
+else
+  echo Copying static site into '$OUTPUT_PATH'...
+  mkdir -p $OUTPUT_PATH
+  cp -v -r _build/html/* $OUTPUT_PATH
+fi
--- content/intro.md
+++ content/intro.md
@@ -4,3 +4,8 @@ This sub-section of the UKWA internal web site contains regularly re-generated r
 
 ```{tableofcontents}
 ```
+
+## Other pages
+
+* [API Collections JSON (including unpublished collections)](./api-json-including-unpublished/)
+* [Automated Test Reports](./test-reports/)
